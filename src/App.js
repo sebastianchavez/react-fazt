9f4c69f53bc3845c99266213dc58b48a279cc93a
Sebastian Chavez <seba.chavez21@gmail.com>
2019-06-21 00:30:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation title="asd"/>
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
